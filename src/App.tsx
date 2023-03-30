a6e0e85a8d8265dacd980c1bfe7f395be1615a97
FranAM1 <falmansamartinez@cifpfbmoll.eu>
2023-03-30 20:39:36 +0200

    Cambiar input

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,24 @@ import './App.css'
 function App() {
   const [cells, setCells] = useState(["A", "B", "C"])
 
+  function changeContent(newValue: string, index: number) {
+    console.log(newValue, index)
+    const nextValue = cells.slice()
+    nextValue[index] = newValue
+    setCells(nextValue)
+  }
+
   return (
     <>
     <h1 className='title'>Primera app de prueba React</h1>
       <div className="cellsContainer">
         {
         cells.map((cell, index) => (
-          <div className="cell" key={index}>
-            {cell}
+          <div 
+          className="cell" 
+          key={index}
+          >
+            <input type="text" maxLength={1} value={cell} onChange={(e) => changeContent(e.currentTarget.value, index)}/>
           </div>
         ))
         }
