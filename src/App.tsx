3c6fd37c3f09d833bd72b95a59babd74aaa51e84
FranAM1 <falmansamartinez@cifpfbmoll.eu>
2023-04-01 13:49:33 +0200

    añadir celda

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState,  } from 'react'
 import './App.css'
 
 function App() {
   const [cells, setCells] = useState(["A", "B", "C"])
 
   function changeContent(newValue: string, index: number) {
-    console.log(newValue, index)
     const nextValue = cells.slice()
     nextValue[index] = newValue
     setCells(nextValue)
   }
+
+  function addCell(index: number) {
+    const nextValue = cells.slice()
+    nextValue.splice(index + 1, 0, "")
+    setCells(nextValue)
+  }
+
+  function createString(e: KeyboardEvent) {
+    if (e.key !== "Enter") return
+
+    console.log(cells.join(""))
+  }
+  
+  useEffect(() => {
+    document.addEventListener("keypress", createString)
+
+    return () => {
+      document.removeEventListener("keypress", createString)
+    }
+  }, [cells])
+
 
   return (
     <>
@@ -17,12 +37,16 @@ function App() {
       <div className="cellsContainer">
         {
         cells.map((cell, index) => (
-          <div 
-          className="cell" 
-          key={index}
-          >
-            <input type="text" maxLength={1} value={cell} onChange={(e) => changeContent(e.currentTarget.value, index)}/>
-          </div>
+          <>
+            <div 
+            className={`cell ${cell === "" ? "emptyCell" : ""}`} 
+            key={index}
+            >
+              <input type="text" maxLength={1} value={cell} onChange={(e) => changeContent(e.currentTarget.value, index)}/>
+            </div>
+            {index < cells.length && 
+            <span className= "addCell" onClick={() => addCell(index)}></span>}
+          </>
         ))
         }
       </div>
